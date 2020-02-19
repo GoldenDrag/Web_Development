356e22d7d541077cc73fccee4f7838069d578759
GoldenDrag <mr.ablaysan@gmail.com>
2020-02-19 12:12:46 +0600

    my first app +-

---
+++ week4/src/app/categories/categories.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+// import { ProductItemComponent } from '../product-item/product-item.component';
+
+@Component({
+  selector: 'app-categories',
+  templateUrl: './categories.component.html',
+  styleUrls: ['./categories.component.css']
+})
+export class CategoriesComponent implements OnInit {
+//   products: products[];
+//   selectedProduct: product;
+  constructor(/*private service: product*/) { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ week4/src/app/product-item/product-item.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-item',
+  templateUrl: './product-item.component.html',
+  styleUrls: ['./product-item.component.css']
+})
+export class ProductItemComponent implements OnInit {
+  products = products;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  share(): void{
+    window.alert('The product has been shared!');
+  }
+
+}
